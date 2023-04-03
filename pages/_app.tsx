883d2482e3d070ac735552197bd77d3a5ca6ae03
Anthony Chablov <achablov@myseneca.ca>
2023-04-03 00:39:14 -0400

    initial config

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import {Inter} from 'next/font/google';
+import type { AppProps } from 'next/dist/shared/lib/router/router';
+const inter = Inter({subsets: ['latin']});
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+    return (
+        <div className={inter.className}>
+            <Head>
+                <title key={'title'}>Create Next App</title>
+                <meta name="description" key={'description'} content="Next App"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title key={'title'}>Anthony Chablov</title>
+      </Head>
+      <div className="h-screen bg-blue-300">
+        123
+      </div>
+    </>
+  )
+}
