062bb6738c17d2a0361c2e55bbf848c7ecd5275e
Anthony Chablov <achablov@myseneca.ca>
2023-04-15 23:30:05 -0400

    add custom scroll bar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,8 @@ import {Html, Head, Main, NextScript} from 'next/document';
 
 export default function _document() {
   return (
-    <Html lang='en' className='scroll-smooth scrollbar-thin scrollbar-thumb-slate-600 scrollbar-track-slate-200  overflow-y-scroll'>
+    <Html lang='en' 
+      className='scroll-smooth scrollbar-thin scrollbar-thumb-slate-600 scrollbar-track-slate-300 overflow-y-scroll'>
         <Head/>
         <body >
             <Main/>
