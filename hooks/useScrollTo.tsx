8efe1376a6fb9d5c3f68035a83eda926c35bc88f
Anthony Chablov <achablov@myseneca.ca>
2023-07-14 10:17:19 -0400

    changes to resume and scroll to top

--- hooks/useScrollTo.tsx
+++
@@ -1,6 +0,0 @@
-import { useEffect } from "react"
-export const useScrollTo = (x: number, y: number) => {
-    useEffect(() => {
-        window.scrollTo(x,y)
-    })
-}
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -8,18 +8,20 @@ import Sidebar from '../components/Sidebar/Sidebar';
 import PDFEmbed from '../components/PDFEmbed/PDFEmbed';
 import Footer from '../components/Footer/Footer';
 import myResume from '../public/resume/resume.pdf';
-import { useScrollTo } from '../hooks/useScrollTo';
+import useScrollToTop from '../hooks/useScrollToTop';
 import Header from '../components/Common/Header';
 
 const resume = () => {
-
+    
+    // State
     const theme = useStateStore(state => state.theme);
     const setTheme = useStateStore(state => state.setTheme);
 
-    useScrollTo(0,0);
+    // Hooks
+    const windowWidth = useWindowWidth();
+    useScrollToTop();
 
     useEffect(() => {
-
         const themeMode = localStorage.getItem("theme-mode-ac");
     
         if (theme === 'dark' || themeMode === 'dark' ) {
@@ -29,38 +31,37 @@ const resume = () => {
           document.documentElement.classList.remove('dark');
           setTheme('light');
         }
-    
       }, [theme]);
 
-    const windowWidth = useWindowWidth();
+   
     return (
-        <>
-        <LazyMotion features={domAnimation}>
-            <div className="dark:bg-black ">
-                <Head>
-                    <title key={'title'}>Anthony Chablov</title>
-                </Head>
-                <div className="bg-zinc-100 dark:bg-slate-800 ">
-                    <NavBar/>
-                    <div className="h-fit px-10 mx-auto max-w-5xl">
-                        <div className="flex items-center">
-                            <Header title={'My CV'}/>
+        <div >
+            <LazyMotion features={domAnimation}>
+                <div className="dark:bg-black ">
+                    <Head>
+                        <title key={'title'}>Anthony Chablov</title>
+                    </Head>
+                    <div className="bg-zinc-100 dark:bg-slate-800 ">
+                        <NavBar/>
+                        <div className="h-fit px-10 mx-auto max-w-5xl">
+                            <div className="flex items-center">
+                                <Header title={'My CV'}/>
+                            </div>
+                            <PDFEmbed pdfUrl={myResume} />
+                            <Footer/>
                         </div>
-                        <PDFEmbed pdfUrl={myResume} />
-                        <Footer/>
                     </div>
+                {
+                    windowWidth >= 1280 && (
+                        <>
+                            <Sidebar anchor={'left'} mode={'icons'}/>
+                            <Sidebar anchor={'right'} mode={'email'}/>
+                        </>
+                    )
+                }
                 </div>
-            {
-                windowWidth >= 1280 && (
-                    <>
-                        <Sidebar anchor={'left'} mode={'icons'}/>
-                        <Sidebar anchor={'right'} mode={'email'}/>
-                    </>
-                )
-            }
-            </div>
-        </LazyMotion>
-        </>
+            </LazyMotion>
+        </div>
     )
 }
 
