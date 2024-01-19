674ea29394f1357307eb7273b370bfb777f1d541
Anthony Chablov <achablov@myseneca.ca>
2024-01-19 11:00:30 -0500

    feat: add background style to body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,44 @@
-import {Inter} from 'next/font/google';
-import type { AppProps } from 'next/dist/shared/lib/router/router';
-import '../styles/globals.css';
-const inter = Inter({subsets: ['latin']});
-import Head from 'next/head';
-import { Analytics } from '@vercel/analytics/react';
+import { Inter } from "next/font/google";
+import type { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/globals.css";
+const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
-    return (
-        <div className={inter.className}>
-            <Head>
-                <title key={'title'}>Anthony Chablov</title>
-                <meta name="description" key={'description'} content="Welcome To My Portfolio Site!"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-            </Head>
-            <Component {...pageProps} />
-            <Analytics />
-        </div>
-    );
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title key={"title"}>Anthony Chablov</title>
+        <meta
+          name="description"
+          key={"description"}
+          content="Welcome To My Portfolio Site!"
+        ></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+      <Analytics />
+    </div>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,16 @@
-import {Html, Head, Main, NextScript} from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function _document() {
   return (
-    <Html lang='en' 
-      className='scroll-smooth scrollbar-thin scrollbar-thumb-slate-600 scrollbar-track-slate-300 overflow-y-scroll'>
-        <Head/>
-        <body className=''>
-            <Main/>
-            <NextScript/>
-        </body>
+    <Html
+      lang="en"
+      className="scroll-smooth scrollbar-thin scrollbar-thumb-slate-600 scrollbar-track-slate-300 overflow-y-scroll bg-[#0f172a] "
+    >
+      <Head />
+      <body className="dark:bg-slate-800 bg-slate-200">
+        <Main />
+        <NextScript />
+      </body>
     </Html>
-  )
+  );
 }
